58d1704435e9af3868daaa605651c9ee750877a6
Juily1992 <juilliet1992@GMAIL.COM>
2024-10-03 22:16:56 +0300

    Revert "Проверьте ДЗ,пожалуйста"

--- src/Main.java
+++ src/Main.java
@@ -4,52 +4,14 @@ public class Main {
     public static void main(String[] args) {
         // Press Alt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
+        System.out.print("Hello and welcome!");
+
         // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        // Task 1
-        var dog = 8;
-        var cat = 3.6;
-        var paper = 763789;
+        for (int i = 1; i <= 5; i++) {
 
-        System.out.println(dog);
-        System.out.println(cat);
-        System.out.println(paper);
-
-        System.out.println(dog + 4);
-        System.out.println(cat + 4);
-        System.out.println(paper +4);
-
-        System.out.println(dog + 3.5);
-        System.out.println(cat - 1.6);
-        System.out.println(paper - 7639);
-
-        var friend = 19;
-        var friend1 = friend +2;
-        var friend2 = friend1 / 7;
-         System.out.println(friend);
-         System.out.println(friend1);
-        System.out.println(friend2);
-
-        var frog = 3.5;
-        System.out.println(frog);
-        System.out.println(frog * 10);
-        System.out.println((frog * 10) / 3.5 );
-        System.out.println(((frog * 10) / 3.5) + 4);
-
-        var boxer1 = 78.2;
-        var boxer2 = 82.7;
-        System.out.println(boxer1 + boxer2 + "кг");
-        System.out.println(Math.abs(boxer1 - boxer2) + "кг");
-
-                System.out.println( boxer2 % boxer1);
-
-        var workers = 640 / 8;
-        System.out.println("Всего работников в компании — " + workers + " человек");
-        var workers1 = workers+94;
-        System.out.println( "Если в компании работает " + workers1 + " человек, то всего " + workers1*8 + " часа работы может быть поделено между сотрудниками»");
-
-        
-
+            // Press Shift+F9 to start debugging your code. We have set one breakpoint
+            // for you, but you can always add more by pressing Ctrl+F8.
+            System.out.println("i = " + i);
+        }
     }
-
-    }
-
+}
